540471346d6befe6839985246c53491d1c8bd99e
Piljae Kang <72394357+Piljae-Kang@users.noreply.github.com>
2025-10-09 00:33:16 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1095ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 251009/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[101][101];
+
+bool route = false;
+
+int move_func[2][2] = {{1, 0}, {0, 1}};
+
+int visited[101][101] = {0};
+
+void dfs(int u, int v){
+
+    if(u == n-1 && v == m-1){
+        
+        route = true;
+        return;
+    }
+
+    visited[u][v] = 1;
+
+
+    for(int i=0; i<2; i++){
+
+        int n_u = u + move_func[i][0];
+        int n_v = v + move_func[i][1];
+
+        if(n_u < 0 || n_v < 0 || n_u >= n || n_v >= m) continue;
+
+        if(grid[n_u][n_v] == 0) continue;
+
+        if(visited[n_u][n_v] == 1) continue;
+
+        dfs(n_u, n_v);
+
+        visited[n_u][n_v] = 0;
+    }
+
+
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+   dfs(0, 0);
+
+   if(route == true) cout << "1\n";
+   else cout << "0\n";
+
+    return 0;
+}
