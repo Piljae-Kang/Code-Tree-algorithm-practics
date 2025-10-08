aa1afad285758ed316cbbdadc7b6e9e1ea765c9e
Piljae Kang <72394357+Piljae-Kang@users.noreply.github.com>
2025-10-08 17:20:33 +0900

    Title: 사다리 타기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251008/사다리 타기/ladder-game.cpp
+++ 251008/사다리 타기/ladder-game.cpp
@@ -1,177 +1,74 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+
+#define MAX_N 11
 
 using namespace std;
 
 int n, m;
 
-int max_depth = 0;
+vector<pair<int, int> > lines;
+vector<pair<int, int> > selected_lines;
 
-int a[15], b[15];
+int ans = INT_MAX;
 
-int result[16] = {0};
-int new_result[16] = {0};
-
-int min_value = 15*15;
-
-
-void ladder_game(int ladder[][16], int height, int width, int type){
-
-	// cout << "------------\n";
-
-	if(type == 1){
-
-		for(int i = width; i<=n; i++){
-			ladder[height][i] = 0;
-		}
+// 처음 상황과, 선택한 가로줄만 사용했을 때의
+// 상황을 시뮬레이션하여
+// 둘의 결과가 같은지 확인합니다.
+bool Possible() {
+    // Step1. 시작 숫자를 셋팅합니다.
+	int num1[MAX_N], num2[MAX_N];
+	for(int i = 0; i < n; i++)
+		num1[i] = num2[i] = i;
 	
-		for(int i=height+1; i<=max_depth; i++){
-			for(int j=1; j<=n; j++){
-	
-				ladder[i][j] = 0;
-			}
-		}
-
+    // Step2. 위에서부터 순서대로 적혀있는 
+    // 가로줄에 대해 양쪽 번호에 해당하는 숫자를 바꿔줍니다. 
+	for(int i = 0; i < (int) lines.size(); i++) {
+		int idx = lines[i].second;
+		swap(num1[idx], num1[idx + 1]);
 	}
+	for(int i = 0; i < (int) selected_lines.size(); i++) {
+		int idx = selected_lines[i].second;
+		swap(num2[idx], num2[idx + 1]);
+	}
+	
+    // Step3. 두 상황의 결과가 동일한지 확인합니다.
+	for(int i = 0; i < n; i++)
+		if(num1[i] != num2[i])
+			return false;
 
-	// for(int i=1; i<=n;i++){
-	// 	for(int j=1; j<=max_depth; j++){
-
-	// 		if(ladder[j][i] == 1){
-	// 			cout << i << ", " << j << "\n";
-	// 		}
-	// 	}
-	// }
-
-    
-    for(int i=1; i<=n; i++){
-
-        int currLine = i;
-
-        for(int step = 1; step <= max_depth; step++){
-
-            for(int dir = currLine -1; dir<= currLine; dir++){
-
-                if(dir < 1 || dir >= n) continue;
-
-                if (ladder[step][dir] == 1){
-
-                    if (dir == currLine) currLine++;
-                    else currLine--;
-
-                    break;
-                }
-
-            }
-        }
-		new_result[i] = currLine;
-        // cout << currLine << " ";
-    }
-
-
-	// cout << "\n";
-	// cout << "------------\n";
+	return true;
 }
 
-void find_new_ladder(int ladder_info[][16], int line, int depth){
-
-
-    int width = depth % (n-1);
-    int height = depth / (n-1);
-	width++;
-	height++;
-
-
-	ladder_game(ladder_info, height, width, 1);
-
-	int cnt = 0;
-	for(int i=1; i<=n; i++){
-
-		if(result[i] == new_result[i]) {
-
-			cnt++;
-		}
-	}
-
-	if(cnt == n){
-
-
-		// for(int i=1; i<=n;i++){
-		// 	for(int j=1; j<=max_depth; j++){
-
-		// 		if(ladder_info[j][i] == 1){
-		// 			cout << i << ", " << j << "\n";
-		// 		}
-		// 	}
-		// }
-
-		// cout <<"found !! : " << line << "\n";
-		min_value = min(min_value, line);
-
-		// return;
-	}
-
-
-	if(depth > (n-1)* max_depth){ // 마지막 ladder
+void FindMinLines(int cnt) {
+	if(cnt == m) {
+		if(Possible())
+			ans = min(ans, (int) selected_lines.size());
 		return;
 	}
-
-	int previous_width = width - 1;
-
-	if(previous_width == 0){
-
-		ladder_info[height][width] = 0;
-		find_new_ladder(ladder_info, line, depth + 1);
-
-		ladder_info[height][width] = 1;
-		find_new_ladder(ladder_info, line + 1, depth + 1);
-
-	}
-	else{
-
-		if (ladder_info[height][width - 1] == 1) { // 
-
-			ladder_info[height][width] = 0;
-			find_new_ladder(ladder_info, line, depth + 1);
-
-		}
-		else{ 
-
-			ladder_info[height][width] = 1;
-			find_new_ladder(ladder_info, line + 1, depth + 1);
 	
-		}
-
-	}
-
+	selected_lines.push_back(lines[cnt]);
+	FindMinLines(cnt + 1);
+	selected_lines.pop_back();
+	
+	FindMinLines(cnt + 1);
 }
 
-
 int main() {
-    cin >> n >> m;
-
-    int ladder_info[16][16] = {0};
-
-    for (int i = 0; i < m; i++) {
-        cin >> a[i] >> b[i];
-
-        ladder_info[b[i]][a[i]] = 1;
-
-		max_depth = max(max_depth,  b[i]);
-    }
-
-    ladder_game(ladder_info, 0, 0, 0);
-
-	for(int i=1; i<=n; i++){
-		result[i] = new_result[i];
+	cin >> n >> m;
+	
+	for(int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		lines.push_back(make_pair(b, a - 1));
 	}
+	
+	sort(lines.begin(), lines.end());
 
-	min_value = m;
-
-	int new_ladder_info[16][16] = {0};
-
-	find_new_ladder(new_ladder_info, 0, 0);
-
-	cout << min_value << "\n";
-
-    return 0;
+	FindMinLines(0);
+	
+	cout << ans;
+	return 0;
 }
