a80e38c7e26ed91bc712af3df873718bfa2f553d
Piljae Kang <72394357+Piljae-Kang@users.noreply.github.com>
2025-10-15 19:40:52 +0900

    Title: 사각형 채우기 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251015/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+
+    cin >> n;
+
+    int arr[1001];
+
+    arr[1] = 2;
+
+    for(int i=2; i<=n; i++){
+
+        arr[i] = 3 * arr[i-1] + 1;
+
+    }
+
+    cout << arr[n] << "\n";
+
+
+    return 0;
+}
